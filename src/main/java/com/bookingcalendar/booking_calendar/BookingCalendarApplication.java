e59ff7ea7ee8ff0f17dc4d2fbe893f3070bf14e7
Nannapat <NannapatSrimartpirom@gmail.com>
2025-11-16 21:55:13 +0700

    Initial commit of Spring Boot project structure via Initializr

---
+++ src/main/java/com/bookingcalendar/booking_calendar/BookingCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.bookingcalendar.booking_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookingcalendar/booking_calendar/BookingCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingcalendar.booking_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
